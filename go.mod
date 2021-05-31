919c346807a8434301a68e3df0e904b0d9dd4cfe
leiyudaye <2638367583@qq.com>
2021-05-31 20:48:41 +0800

    [ADD] 增加Http反向代理

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/leiyudaye/gateway
+module gateway
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/hashicorp/consul/api v1.8.1
+)
--- main.go
+++ main.go
@@ -1,30 +1,67 @@
+/*
+ * @Descripttion:
+ * @Author: lly
+ * @Date: 2021-05-28 21:09:55
+ * @LastEditors: lly
+ * @LastEditTime: 2021-05-31 20:47:00
+ */
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	reverse "gateway/reverseproxy"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-kit/kit/log"
+	"github.com/hashicorp/consul/api"
 )
 
-type Gateway struct {
-	Comm      string `form:"comm" json:"comm" uri:"comm" xml:"comm" binding:"required"`
-	ModuleKey string `form:"moduleKey" json:"moduleKey" uri:"moduleKey" xml:"moduleKey" binding:"required"`
-}
+var logger log.Logger
 
-func HanleGateway(c *gin.Context) {
-	var json map[string]interface{}
-	if err := c.ShouldBindJSON(&json); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	fmt.Println(json)
-	c.JSON(http.StatusOK, gin.H{"status": "200"})
+func init() {
+	// 创建日志组件
+	logger = log.NewLogfmtLogger(os.Stderr)
+	logger = log.With(logger, "Ts", log.DefaultTimestampUTC)
+	logger = log.With(logger, "Caller", log.DefaultCaller)
 }
 
 func main() {
-	route := gin.Default()
-	route.POST("cgi-bin/gateway.fcg", HanleGateway)
-	route.Run(":8000")
+	// 创建环境变量
+	var (
+		consulHost = flag.String("consul.host", "127.0.0.1", "consul server ip address")
+		consulPort = flag.String("consul.port", "8500", "consul server port")
+	)
+	flag.Parse()
+
+	// 创建consul api 客户端
+	consulConfig := api.DefaultConfig()
+	consulConfig.Address = "http://" + *consulHost + ":" + *consulPort
+	conCli, err := api.NewClient(consulConfig)
+	if err != nil {
+		logger.Log("err", err)
+		os.Exit(1)
+	}
+
+	// 创建反向代理
+	proxy := reverse.NewHttpReverseProxy(conCli, logger)
+
+	errc := make(chan error)
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errc <- fmt.Errorf("%s", <-c)
+	}()
+
+	// 开始监听
+	go func() {
+		logger.Log("listen...", ":9090")
+		errc <- http.ListenAndServe(":9090", proxy)
+	}()
+
+	logger.Log("exit", <-errc)
 }
